11f93bee9d7c04ed76d3a5786b83408462bf1e3a
김어진 <kimuj5090@gmail.com>
2022-10-15 02:33:24 +0900

    build

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
 };
 
 const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
   </div>
 );
 
