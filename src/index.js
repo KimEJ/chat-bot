00837387e82a2865cc9a1aec7346580aaf62d458
김어진 <kimuj5090@gmail.com>
2022-11-11 01:09:12 +0900

    add close button

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 
-const styles = {
-};
+// function close() {
+//   console.log("clicked");
+//   window.parent.postMessage(
+//     {
+//       type: "close",
+//     },
+//     "*"
+//   );
+// }
 
 const App = () => (
   <div>
+    <button type="button" className={"btn-close"} onClick={() => {
+      console.log("clicked");
+      window.parent.postMessage(
+        {
+          type: "close",
+        },
+        "*"
+      );
+    }} style={{float: "right"}} aria-label="Close"></button>
     <Hello name="CodeSandbox" />
+
   </div>
 );
 
